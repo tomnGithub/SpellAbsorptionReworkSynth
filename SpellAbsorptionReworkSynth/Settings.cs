e97eb53ea2256dbff3d1d6782f21c2ea1fb5f159
tomnGithub <165872442+tomnGithub@users.noreply.github.com>
2025-02-26 15:06:28 -0800

    Add files via upload

---
+++ SpellAbsorptionReworkSynth/Settings.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // Add this namespace
+using Mutagen.Bethesda.Plugins;
+using System.ComponentModel;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+using Synthesis.Bethesda.Commands;
+
+namespace SpellAbsorptionReworkSynth
+{
+    public record Settings
+    {
+		
+
+
+    }
+
+    
+
+
+
+
+
+}
+
